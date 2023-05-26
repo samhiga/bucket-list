cb3c1891b639b5ea6c8e3744be4892f52bf31539
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-05-26 14:17:52 -0700

    more code added

--- src/components/Bucket.js
+++ src/components/Bucket.js
@@ -14,8 +14,9 @@ function Bucket(props) {
 
     // TODO: Write logic to update the `edit` value in state after a user updates an entry in the list
 
+    setEdit()
     // TODO: Set the key:value pairs in the `edit` object back to empty strings
-
+    
   };
 
   // If the user is attempting to edit an item, render the bucket form with the edit variable passed as a prop
@@ -27,10 +28,10 @@ function Bucket(props) {
     // TODO: Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
     // TODO: Add a key attribute set to the value of the index position
     // Hint: use a ternary operator
-    <div className={ } key={}>
+    <div className={`${item.complete }`} key={index}>
 
       // TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument
-      <div key={} onClick={}>
+      <div key={index} onClick={}>
           {/* TODO: Add the item text here */}
       </div>
       <div className="icons">
--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -7,7 +7,7 @@ function BucketList() {
 
   // Function to add a bucket list item
   const addBucketItem = (item) => {
-
+    return setBucket((bucket.push(item)))
     // TODO: Write logic to add the new bucket item to the bucket state variable
     
   };
@@ -16,9 +16,13 @@ function BucketList() {
   const completeBucketItem = (id) => {
     // If the ID passed to this function matches the ID of the item that was clicked, mark it as complete
     let updatedBucket = bucket.map((item) => {
-      
+      if(item.id == id){
+        //"mark that item as complete"
+        //item.complete = true;
+        return {...item, complete: true }
+      }
       // TODO: Write logic that marks an item as complete or incomplete when invoked
-
+      return item;
     });
 
     setBucket(updatedBucket);
@@ -27,9 +31,13 @@ function BucketList() {
   // Function to remove bucket list item and update state
   const removeBucketItem = (id) => {
     // TODO: Write logic that will return an array of items that don't contain the ID passed to this function
+    let removeBucket = bucket.filter((item) => item.id !== id);
+    //it's an array
+    //we can use "filter" to remove items from an array.$
 
+    // let updatedBucket2 = bucket.filter((item) => item.id == id)
 
-    // TODO: Update the bucket state variable
+    return setBucket(removeBucket);
   };
 
   // Function to edit the bucket list item
